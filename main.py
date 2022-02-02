4a9228b869fa78862dc335cf8521b7f60aa54195
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-02 03:07:11 +0300

    all news articles added

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   newsapi = NewsApiClient(api_key="eee9e812d2274500b4bb312f3a572be0")
-  top_headlines= newsapi.get_top_headlines(sources='bbc-news')
+  top_headlines= newsapi.get_top_headlines(sources= 'bbc-news')
+  all_articles= newsapi.get_everything(sources='bbc-news')
+  
+  
 
   top_articles= top_headlines['articles']
+  a_articles= all_articles['articles']
 
   news=[]
   img=[]
   url=[]
   desc=[]
   publish_date=[]
+
+  
 
   for i in range(len(top_articles)):
     main_article=top_articles[i]
@@ -25,14 +31,31 @@ def index():
     publish_date.append(main_article['publishedAt'])
     url.append(main_article['url'])
 
-    contents= zip(news,desc,img,publish_date,url)
+
+  news_all=[]
+  img_all=[]
+  url_all=[]
+  desc_all=[]
+  publish_date_all=[]
+
+  for l in range(len(a_articles)):
+    a_article=a_articles[l]
+
+    news_all.append(a_article['title'])
+    desc_all.append(a_article['description'])
+    img_all.append(a_article['urlToImage'])
+    publish_date_all.append(a_article['publishedAt'])
+    url_all.append(a_article['url'])
+
+  contents= zip(news,desc,img,publish_date,url)
+  all= zip(news_all,desc_all,img_all,publish_date_all,url_all)
 
 
 
-    return render_template('index.html',contents=contents)
+  return render_template('index.html',contents=contents,all=all)
 
 
-  return render_template('index.html')
+  # return render_template('index.html')
 
   if __name__=='__main__':
    app.run(debug=True)
